5afe5f1d5c094ec9698f2ae6a6adc08319eaced6
tm <tamorimoto@hotmail.com>
2021-08-22 21:24:59 +0900

    rewrite for gradle

---
+++ src/main/java/seamshop/action/HomeAction.java
@@ -0,0 +1,25 @@
+package seamshop.action;
+
+import org.apache.struts2.interceptor.validation.SkipValidation;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionSupport;
+
+import seamshop.consts.Spring;
+
+@Component
+@Scope(Spring.ACTION_SCOPE)
+@SuppressWarnings("serial")
+public class HomeAction extends AbstractGuestAction
+{
+	@SkipValidation
+	@Override
+	public String execute() throws Exception
+	{
+		log.debug("call execute()");
+
+		return SUCCESS;
+	}
+}
